3dbb9911f17e1bc613c3264e434d38c38de1c35d
Carlos John Escala <99855145+cjaeey@users.noreply.github.com>
2024-10-08 21:59:34 -0700

    Add files via upload

---
+++ CubeProgram.java
@@ -0,0 +1,24 @@
+// Carlos John Escala
+// August 14, 2024
+// Lab 1, Cube Building With Text
+// This program creates a Cube
+public class CubeProgram
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("*****************");
+        System.out.println ("*               * *");
+        System.out.println ("*               *  *");
+        System.out.println ("*               *   *");
+        System.out.println ("*               *    *");
+        System.out.println ("*               *    *");
+        System.out.println ("*               *    *");
+        System.out.println ("*               *    *");
+        System.out.println ("*****************    *");
+        System.out.println (" *               *   *");
+        System.out.println ("  *               *  *");
+        System.out.println ("   *               * *");
+        System.out.println ("    *               **");
+        System.out.println ("     *****************");
+    }
+}
